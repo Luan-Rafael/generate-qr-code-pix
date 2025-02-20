bdc2aab92e24706d6ee42cdab43a232cd17ec952
Luan-Rafael <luan.tec18@gmail.com>
2025-02-19 22:17:43 -0300

    feat: configuration primevue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/lara'; 
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App); 
+
+app.use(PrimeVue, {
+    theme: {
+        preset:Aura
+    }
+});
+
+app.mount('#app');
